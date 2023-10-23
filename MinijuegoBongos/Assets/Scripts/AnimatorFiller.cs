b44e95d1599e1772bc92a4eb58efd8230b5560b4
JoseMFT <jomaft03@gmail.com>
2023-10-23 13:58:16 +0100

    23/10/2023
    Avances en el texturizado del modelo y las partículas, aunque no se reinicia el tamaño de las perlas al volver a su posición inicial, al igual que tampoco suben los puntos como se supone que deberían hacerlo.

---
+++ MinijuegoBongos/Assets/Scripts/AnimatorFiller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatorFiller : MonoBehaviour
+{
+    public GameObject particleChangeVal;
+    Slider selfSlider;
+    GameObject estrellaFinal;
+
+    private void Awake()
+    {
+        selfSlider = gameObject.GetComponent<Slider>();
+        estrellaFinal = GameObject.Find("Estrella llena");
+        estrellaFinal.SetActive(false);
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selfSlider.value >= 1f && estrellaFinal.activeSelf == false) estrellaFinal.SetActive(true);   
+    }
+
+    public void animationLT()
+    {
+        GameObject.Instantiate(particleChangeVal, estrellaFinal.transform.position, Quaternion.identity);
+        LeanTween.scale(gameObject, Vector3.one, .5f).setEaseInOutElastic();
+    }
+}
