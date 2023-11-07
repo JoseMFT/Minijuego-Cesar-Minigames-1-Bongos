e2e71f454c57f4ae76ef9780dc392d63725ebc32
JoseMFT <jomaft03@gmail.com>
2023-11-07 13:28:34 +0000

    07/11/2023 - 13:28
    Renderizar e incorporar dentro del juego

--- MinijuegoBongos/Assets/Scripts/CameraMovStart.cs
+++ MinijuegoBongos/Assets/Scripts/CameraMovStart.cs
@@ -6,30 +6,50 @@ using UnityEngine.UI;
 public class CameraMovStart : MonoBehaviour
 {
     GameObject menuCanvas;
-    float speed = 5f;
+    float velocidad = 1.25f;
     CanvasGroup menuCanvasGroup;
-    Vector3 newCameraPos, cameraPos, newCameraRot, cameraRot;
-    public bool animate = false;
+    public Vector3 nuevaPos, cameraPos;
+    bool animar = false, mismaPos = false, mismaRot = false;
+    public Quaternion nuevaRot, cameraRot;
 
     private void Awake () {
-        newCameraPos = GameObject.Find ("GameCameraPos").transform.position;
-        newCameraRot = GameObject.Find ("GameCameraPos").transform.rotation.eulerAngles;
+        nuevaPos = GameObject.Find ("GameCameraPos").transform.position;
+        nuevaRot = GameObject.Find ("GameCameraPos").transform.rotation;
         menuCanvas = GameObject.Find ("Canvas");
         menuCanvasGroup = menuCanvas.GetComponent<CanvasGroup> ();
     }
     private void Update () {
 
         cameraPos = transform.position;
-        cameraRot = transform.rotation.eulerAngles;
+        cameraRot = transform.rotation;
 
-        if (animate == true && cameraPos != newCameraPos && cameraRot != newCameraRot) {
-            transform.position = Vector3.MoveTowards(cameraPos, newCameraPos, speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.MoveTowards (cameraRot, newCameraRot, speed * Time.deltaTime));
-        } else {
-            animate = false;
-            menuCanvas.GetComponent <GraphicRaycaster>().enabled = false;
-            LeanTween.alphaCanvas (menuCanvasGroup, 0f, 1f);
+        if (animar == true) {
+
+            if (cameraPos != nuevaPos) {
+                transform.position = Vector3.MoveTowards (cameraPos, nuevaPos, velocidad * Time.deltaTime);
+            } else {
+                mismaPos = true;
+            }
+
+            if (cameraRot != nuevaRot) {
+                transform.rotation = Quaternion.RotateTowards (cameraRot, nuevaRot, 3.575f * velocidad * Time.deltaTime);
+
+                if (Quaternion.Angle (cameraRot, nuevaRot) < .2f)
+                    transform.rotation = nuevaRot;
+
+            } else {
+                mismaRot = true;
+            }
+
+            if (mismaPos == true && mismaRot == true)
+                animar = false;
         }
+    }
+
+    public void AnimacionQuitarCanvas () {
+        animar = true;
+        menuCanvas.GetComponent<GraphicRaycaster> ().enabled = false;
+        LeanTween.alphaCanvas (menuCanvasGroup, 0f, 2f);
     }
 
 
