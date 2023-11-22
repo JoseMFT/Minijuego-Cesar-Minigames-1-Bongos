d73503b4eb52f297c75da5dd05dc2ff9197d71b3
JoseMFT <jomaft03@gmail.com>
2023-11-22 19:14:13 +0000

    22/11/2023 - 19:13
    Save pre-psico

--- MinijuegoBongos/Assets/Scripts/GameManager.cs
+++ MinijuegoBongos/Assets/Scripts/GameManager.cs
@@ -12,15 +12,18 @@ public class GameManager : MonoBehaviour
     public AudioMixer mezclador;
     public Slider barraVolumen;
     public TextMeshProUGUI textoVolumen;
+    public GameObject menuOpciones;
+
     
     // Start is called before the first frame update
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        DetectarSonido();
     }
 
     private void Start () {
-        DetectarSonido();        
+        //DetectarSonido();        
     }
 
     // Update is called once per frame
@@ -33,22 +36,25 @@ public class GameManager : MonoBehaviour
     }
 
     public void DetectarSonido () {
+        menuOpciones = GameObject.Find("FondosOpciones");
         barraVolumen = GameObject.Find("Volumen Barra").GetComponent<Slider>();
         volumenJuego = barraVolumen.value;
-        //mezclador.SetFloat("Volumen", -25f - 25f * ((100f - volumenJuego) / 100f));
+        mezclador.SetFloat("Volumen", 0f - 35f * ((100f - volumenJuego) / 100f));
         textoVolumen = GameObject.Find("Volumen").GetComponent<TextMeshProUGUI>();
-
-        /*barraVolumen.onValueChanged.AddListener(delegate {
-            CambiarVolumen();
-        });*/
+        menuOpciones.SetActive(false);
     }
 
     public void CambiarVolumen () {
         volumenJuego = barraVolumen.value;
-        mezclador.SetFloat("Volumen", -25f - 35f * ((100f- volumenJuego) / 100f));
+        mezclador.SetFloat("Volumen", 0f - 35f * ((100f- volumenJuego) / 100f));
         if (volumenJuego == 0f) {
             mezclador.SetFloat("Volumen", -100f);
         }
         textoVolumen.text = volumenJuego.ToString();
+    }
+
+    public void CambiarDificultad (float nuevaDificultad)
+    {
+        dificultadJuego = nuevaDificultad;
     }
 }
